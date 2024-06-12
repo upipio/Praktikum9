53f0db69bae8e749900664fecebb48308bfd65c2
upi <luthfipaundra01@gmail.com>
2024-06-12 08:08:41 +0700

    Add files via upload

---
+++ Praktikum9B_1402023032/Praktikum9_1402023032/src/praktikum9_1402023032/Praktikum9_1402023032.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum9_1402023032;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Praktikum9_1402023032 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
